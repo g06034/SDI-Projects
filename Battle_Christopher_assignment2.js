a28b89b090d9af8c9f79632402e7355127f54903
blackPh0enix <clbattle90@gmail.com>
2013-10-10 02:17:43 -0500

    Put Basic Info in js

--- Battle_Christopher_assignment2.js
+++ Battle_Christopher_assignment2.js
@@ -1,3 +1,6 @@
 //alert("JavaScript works!");
 
-// Author: Christopher Battle 
+// Author: Christopher Battle // "The Smartphone Swap"
+// Author: Christopher L. Battle
+// SDI 1310 Project 2 
+// Build upon the previous weeks story 
