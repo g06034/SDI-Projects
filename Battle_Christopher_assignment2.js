18ac4cf5c7820d339f67d3ff5fc7f8e9a7e3cb01
blackPh0enix <clbattle90@gmail.com>
2013-10-10 02:20:13 -0500

    Added Basic Info

--- Battle_Christopher_assignment2.js
+++ Battle_Christopher_assignment2.js
@@ -1,6 +1,5 @@
 //alert("JavaScript works!");
-
-// Author: Christopher Battle // "The Smartphone Swap"
+// "The Smartphone Swap"
 // Author: Christopher L. Battle
 // SDI 1310 Project 2 
 // Build upon the previous weeks story 
