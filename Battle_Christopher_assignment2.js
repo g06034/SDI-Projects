2ebcc0674406367824c22137e965c16c3b6d7196
blackPh0enix <clbattle90@gmail.com>
2013-10-10 21:12:11 -0500

    Added Variables

--- Battle_Christopher_assignment2.js
+++ Battle_Christopher_assignment2.js
@@ -1,5 +1,21 @@
-//alert("JavaScript works!");
-// "The Smartphone Swap"
-// Author: Christopher L. Battle
-// SDI 1310 Project 2 
-// Build upon the previous weeks story 
+/* alert("JavaScript works!")
+ "The Morning Grind "
+ Author: Christopher L. Battle
+ SDI 1310 Project 2 
+ Build upon the previous weeks story
+ */
+
+// My variables 
+var myEssentials = [ "Government Contractor ID", "Wallet", "iPhone charger" ];
+var mySandwhich = true;
+var myLunch = [ "Sandwhich", "Chips", "Fruit Cup", "Chewy Bar", "Water Bottle" ];
+var myWife = "Eva";
+
+// My outputs
+console.log("Eva " + "did you make my sandwhich last night" + ".");
+
+
+
+
+
+ 
