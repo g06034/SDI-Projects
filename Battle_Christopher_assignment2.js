7d9d1dc4620fc11d108691b014069ab5d77947f8
blackPh0enix <clbattle90@gmail.com>
2013-10-10 22:06:09 -0500

    Added Functions

--- Battle_Christopher_assignment2.js
+++ Battle_Christopher_assignment2.js
@@ -10,20 +10,40 @@ var myEssentials = [ "Government Contractor ID", "Wallet", "iPhone charger" ];
 var mySandwich = true;
 var myLunch = [ "Sandwhich", "Chips", "Fruit Cup", "Chewy Bar", "Water Bottle" ];
 var myWife = "Eva";
-var ImReady = "That means one less step"
+var imReady = "That means one less step";
+var minUntilArrival = 90;
+var minBeforeShift = 120;
 
 // My outputs
 console.log("My alarm on my iPhone goes off");
 console.log( myWife + " did you make my sandwich last night" + ".");
 
-//conditional
+//Conditionals
 if (myWife + mySandwich == true)
         {
-        ImReady === true
+        imReady === true
         }
         else{
         console.log("I need to make a sandwich for lunch")
-        }
+        };
+        
+//Functions        
+        
+        
+var makeSandwich = function(sandwichMaterials, , tupperwareDish, myLunchbox) {
+	console.log(" I proceed to the kitchen to get " + sandwichMaterials + 
+				" ready to make sandwich then put in " +
+				 + tupperwareDish + "."
+				"Then I can pack it up in" +
+				myLunchbox +
+				" and my sandwich will be complete.");
+				
+if (sandwichMaterials > 0) {
+	console.log(" Time to make my sandwich")
+} else {
+	    console.log( "Oh well/, I will just buy some lunch at work") 
+};				
+
 
 
 
