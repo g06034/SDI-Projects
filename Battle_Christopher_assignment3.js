501cf682e621527d3e92a24b3f902598d5117483
blackPh0enix <clbattle90@gmail.com>
2013-10-17 20:52:28 -0500

    Put in header and welcome alert!

---
+++ Battle_Christopher_assignment3.js
@@ -0,0 +1,7 @@
+/* alert("JavaScript works!")
+ "There is a jumpshot in team "
+ Author: Christopher L. Battle
+ SDI 1310 Project 3 
+ */
+ 
+ alert("Welcome to the journey of a man \, who fell in love with the jumpshot!")
