84b22359699ebe2cf7fb6903589c0795d9aff11e
blackPh0enix <clbattle90@gmail.com>
2013-10-17 21:31:42 -0500

    Variable addition

--- Battle_Christopher_assignment3.js
+++ Battle_Christopher_assignment3.js
@@ -1,10 +1,9 @@
-//alert("JavaScript works!")
 /* "There is a jumpshot in team "
  Author: Christopher L. Battle
  SDI 1310 Project 3 
  */
  
- alert("Welcome to the journey of a man \, who fell in love with the jumpshot!")
+ alert("Welcome to the journey of a man \, w ho fell in love with the jumpshot!");
  
  // Variables
  var myName = "Christopher";
