91aa01cbfec7f16fce186fe4cfc3e9a76e579af5
blackPh0enix <clbattle90@gmail.com>
2013-10-17 21:24:57 -0500

    Variable addition

--- Battle_Christopher_assignment3.js
+++ Battle_Christopher_assignment3.js
@@ -1,7 +1,14 @@
-/* alert("JavaScript works!")
- "There is a jumpshot in team "
+//alert("JavaScript works!")
+/* "There is a jumpshot in team "
  Author: Christopher L. Battle
  SDI 1310 Project 3 
  */
  
  alert("Welcome to the journey of a man \, who fell in love with the jumpshot!")
+ 
+ // Variables
+ var myName = "Christopher";
+ var myGyms = ["Welsch Fitness", "Globo Gym", "Average Joes"] 
+ 
+ 
+ 
