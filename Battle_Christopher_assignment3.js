0a62e69d8dd44068e85935a6ffb71e1eb2015a1f
blackPh0enix <clbattle90@gmail.com>
2013-10-17 22:31:00 -0500

    Turning in as is struggled mightily!

--- Battle_Christopher_assignment3.js
+++ Battle_Christopher_assignment3.js
@@ -3,11 +3,31 @@
  SDI 1310 Project 3 
  */
  
- alert("Welcome to the journey of a man \, w ho fell in love with the jumpshot!");
+
+ 
+ alert("Welcome to the journey of a man \, who fell in love with the jumpshot!");
  
  // Variables
  var myName = "Christopher";
- var myGyms = ["Welsch Fitness", "Globo Gym", "Average Joes"] 
+ var myGyms = ["Welsch Fitness", "Globo Gym", "Average Joes"];
+ var winningScore = 21;
+ var teamNames = ["Nets", "Swish"];
+ var myTeam = "Nets"
+ 
+ 
+ 
+  //Intro
+ 
+ console.log("I need to pick my players for this pickup game" + " my opponent has aleready picked his two players");
+ 
+ 
+ //Conditionals
+ 
+
+ 
+
+  
+ 
  
  
  
