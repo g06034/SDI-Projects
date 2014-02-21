5bc1c38107862b644d29f94e67317b99a06a557e
blackPh0enix <clbattle90@gmail.com>
2014-02-20 21:35:43 -0500

    Initial commit

---
+++ Battle_Christopher_assignment3/json.js
@@ -0,0 +1,46 @@
+/* 
+ Author: Christopher L. Battle
+ SDI 1402 Project 3
+ JSON 
+ */
+
+var json = {
+    "prospects": [
+        {
+            "name": "David",
+            "age": 23,
+            "jumpshot": 9,
+            "teamWorthy": true
+        },
+        {
+            "name": "Allan",
+            "age": 24,
+            "jumpshot": 7,
+            "teamworthy": true
+        },
+        {
+            "name": "Gerald",
+            "age": 25,
+            "jumpshot": 3,
+            "teamworthy": false
+        },
+        {
+            "name": "Michael",
+            "age": 27,
+            "jumpshot": 8,
+            "teamworthy": true
+        },
+        {
+            "name": "Stan",
+            "age": 24,
+            "jumpshot": 4,
+            "teamworthy": false
+        },
+        {
+            "name": "John",
+            "age": 26,
+            "jumpshot": 5,
+            "teamworthy": false }
+           
+    ] 
+    };
