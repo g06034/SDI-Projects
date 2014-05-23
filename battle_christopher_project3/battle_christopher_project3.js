6ccbad4f426f95416f0e448789c762319f496583
Chris "blackph0enix" Battle <clbattle90@fullsail.edu>
2014-05-22 23:52:09 -0400

    Added proper file with proper filename
    Added “project3” file.

---
+++ battle_christopher_project3/battle_christopher_project3.js
@@ -0,0 +1,17 @@
+/*
+ Christopher Battle
+ Project 3
+ SDI 1405
+ May 22, 2014
+ */
+// alert("JavaScript works!");
+// Global Variables
+ var myName = "My name is Chris";
+
+
+
+// Console Inputs
+ var yourName = prompt("What is your name?"); /* This prompt gathers user input*/
+ console.log("Hello, " + yourName );
+
+ console.log(myName);
