ad7978ed11a3a565abfc0668afe461054bf880ab
Chris "blackph0enix" Battle <clbattle90@fullsail.edu>
2014-05-24 18:22:25 -0400

    Changed syntax to be easier on the eyes.
    Added more console nuts and global variables as well.

--- battle_christopher_project3/battle_christopher_project3.js
+++ battle_christopher_project3/battle_christopher_project3.js
@@ -4,14 +4,15 @@
  SDI 1405
  May 22, 2014
  */
+ 
 // alert("JavaScript works!");
+
 // Global Variables
  var myName = "My name is Chris";
-
-
-
-// Console Inputs
  var yourName = prompt("What is your name?"); /* This prompt gathers user input*/
+ var youReady = prompt("Before we begin\, I want to make sure your\'re ready for this journey.");
+ 
+ // Console Inputs
  console.log("Hello, " + yourName );
-
+ console.log("Now that I know your name let me tell you mine.");
  console.log(myName);
