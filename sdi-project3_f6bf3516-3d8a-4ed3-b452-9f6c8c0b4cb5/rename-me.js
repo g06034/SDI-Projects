7ab6a9cfee5539d389a54a81259fb2d375e856a7
Chris "blackph0enix" Battle <clbattle90@gmail.com>
2014-06-19 21:47:26 -0400

    Function added
    Functions added and program for calculation added.

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/rename-me.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/rename-me.js
@@ -28,3 +28,43 @@ var json={
 	decision:""
 };
 
+
+//PROGRAM START
+function start(){
+
+	var n=prompt("What\'s your name? ");//Local variable
+	json.name=n;
+	if(n==null){
+		alert("Bye"); //FINISH
+		return;
+	}
+
+	if(n!=""){
+		name=n;
+		var conf=confirm("Welcome\, "+name+"\nDo you want to start entering your grades?"); //Local variable
+		startEnteringGrades=conf;
+		grades=recordGrades(grades);
+		average=calculateAverage(average);
+		succeeded=success(succeeded);
+		decision=appreciation(decision);
+
+		//OUTPUT
+		alert("Your GPA is "+average);
+		console.log("Your GPA is "+average);
+		alert(decision);
+		console.log(decision);
+
+
+		//Output using json data
+		alert("I will display your output using json in the console below.");
+
+		alert("Hello "+json.name+" your average is "+json.average+" and you "+json.decision);
+		console.log("Here is your ouput using json data");
+		console.log(json.name+" your average is "+json.average+" and you "+json.decision);
+
+
+	}
+}
+
+
+//PROGRAM END
