a766cb73d7134a547a7af898b134ce780edf578d
Chris "blackph0enix" Battle <clbattle90@gmail.com>
2014-08-14 20:59:31 -0400

    Initial commit
    Added javascript file for Project 2.

---
+++ sdi-project2/battle_christopher_assignment2.js
@@ -0,0 +1,13 @@
+//alert("JavaScript works!");
+
+
+  /*
+  Christopher Battle
+  August 14, 2014
+  Project 2
+  An expansion of my weekday morning routine
+  Chris's World 2.0
+  */
+
+
+  //Global Variables(Internal Storage)
